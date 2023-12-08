87913e07d8a57a809dbdb14965542b3cf19991c0
vohizy <99481011+Vohizy@users.noreply.github.com>
2023-12-08 10:50:59 +0300

    refactor: change implementation of newPrime

--- src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
+++ src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
@@ -1,35 +1,12 @@
 package com.poja.prime.endpoint.rest.controller;
 
-import com.poja.prime.PojaGenerated;
-import lombok.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@PojaGenerated
 @RestController
-@Value
 public class PrimeController {
-    @GetMapping("/even")
-    public int ping() {
-
-        int nombreAleatoire;
-
-        do {
-            nombreAleatoire = (int) (Integer.MAX_VALUE * Math.random());
-        } while (!estNombrePremier(nombreAleatoire));
-
-        return  + nombreAleatoire;
-    }
-    // Méthode pour vérifier si un nombre est premier
-    public static boolean estNombrePremier(int nombre) {
-        if (nombre <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(nombre); i++) {
-            if (nombre % i == 0) {
-                return false;
-            }
-        }
-        return true;
+    @GetMapping("/new-prime")
+    public BigInteger newPrime() {
+        return BigInteger.probablePrime(1000, new Random());
     }
 }
