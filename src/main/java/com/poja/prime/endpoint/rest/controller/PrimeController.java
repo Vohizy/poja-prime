e0777bbc9d01f6e36c93ae71a0b7dab5aacf8525
vohizy <99481011+Vohizy@users.noreply.github.com>
2023-12-08 10:55:49 +0300

    Update PrimeController.java

--- src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
+++ src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
@@ -3,6 +3,9 @@ package com.poja.prime.endpoint.rest.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 @RestController
 public class PrimeController {
     @GetMapping("/new-prime")
